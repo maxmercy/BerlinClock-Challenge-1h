8d208b716455b822d8500ca727c798020d3458e4
Maxime Mercy <mxmercy@gmail.com>
2016-12-02 13:12:05 +0100

    correction for size fction

--- Berlin_Clock/berlin clock/src/js/BerlinClock.js
+++ Berlin_Clock/berlin clock/src/js/BerlinClock.js
@@ -1,70 +1,65 @@
 var BerlinClock = {
-    tick: function(){
+    tick: function() {
         var time = new Date(),
             second = time.getSeconds();
-            minute = time.getMinutes();
-            hour = time.getHours();
+        minute = time.getMinutes();
+        hour = time.getHours();
 
-    this.updateHours()
-    this.updateMinutes()
-
-    },
-    updateSeconds: function(second){
-         $('.secondsCircle').toggleClass('yellow');
-    },
-
-    updateHours: function(){
-      nbr = Math.floor(hour/5)
-      nbrYellowFive = $('#hoursOfFive .yellow').size;
-      if (nbr != nbrYellowFive) {
-        if (nbr < nbrYellowFive ) {
-          $("#hoursOfFive .ticker").removeClass('yellow');
-          }
-          $("#hoursOfFive .ticker:lt("+nbr+")").addClass('yellow');
-      }
-
-      nbrRest = hour - nbr*5
-      nbrYellowFiveOne = $('#hoursOfOne .yellow').size();
-      if (nbrRest != nbrYellowFiveOne) {
-        if (nbrRest < nbrYellowFiveOne ) {
-          $("#hoursOfOne .ticker").removeClass('yellow');
-          }
-          $("#hoursOfOne .ticker:lt("+nbrRest+")").addClass('yellow');
-      }
+        this.updateHours()
+        this.updateMinutes()
 
     },
 
-    updateMinutes: function(){
-        nbr = Math.floor(minute/5)
-        nbrYellowFive = $('#minutesOfFive .yellow').size;
+    updateSeconds: function(second) {
+        $('.secondsCircle').toggleClass('yellow');
+    },
+
+    updateHours: function() {
+        nbr = Math.floor(hour / 5)
+        nbrYellowFive = $('#hoursOfFive .yellow').size();
         if (nbr != nbrYellowFive) {
-          if (nbr < nbrYellowFive ) {
-            $("#minutesOfFive .ticker").removeClass('yellow');
+            if (nbr < nbrYellowFive) {
+                $("#hoursOfFive .ticker").removeClass('yellow');
             }
-            $("#minutesOfFive .ticker:lt("+nbr+")").addClass('yellow');
+            $("#hoursOfFive .ticker:lt(" + nbr + ")").addClass('yellow');
         }
+        nbrRest = hour - nbr * 5
+        nbrYellowFiveOne = $('#hoursOfOne .yellow').size();
+        if (nbrRest != nbrYellowFiveOne) {
+            if (nbrRest < nbrYellowFiveOne) {
+                $("#hoursOfOne .ticker").removeClass('yellow');
+            }
+            $("#hoursOfOne .ticker:lt(" + nbrRest + ")").addClass('yellow');
+        }
+    },
 
-        nbrRest = minute - nbr*5
+    updateMinutes: function() {
+        nbr = Math.floor(minute / 5)
+        nbrYellowFive = $('#minutesOfFive .yellow').size();
+        if (nbr != nbrYellowFive) {
+            if (nbr < nbrYellowFive) {
+                $("#minutesOfFive .ticker").removeClass('yellow');
+            }
+            $("#minutesOfFive .ticker:lt(" + nbr + ")").addClass('yellow');
+        }
+        nbrRest = minute - nbr * 5
         nbrYellowFiveOne = $('#minutesOfOne .yellow').size();
         if (nbrRest != nbrYellowFiveOne) {
-          if (nbrRest < nbrYellowFiveOne ) {
-            $("#minutesOfOne .ticker").removeClass('yellow');
+            if (nbrRest < nbrYellowFiveOne) {
+                $("#minutesOfOne .ticker").removeClass('yellow');
             }
-            $("#minutesOfOne .ticker:lt("+nbrRest+")").addClass('yellow');
+            $("#minutesOfOne .ticker:lt(" + nbrRest + ")").addClass('yellow');
         }
-
     },
 
-    updateClock: function(hour, minute, second){
-
+    updateClock: function(hour, minute, second) {
+      console.log()
     },
-
-    reset: function(){
-
+    reset: function() {
     }
 }
 
-setInterval(function(){
-      BerlinClock.tick()
-      BerlinClock.updateSeconds()
- },1000);
+setInterval(function() {
+    BerlinClock.tick()
+    BerlinClock.updateSeconds()
+}, 1000);

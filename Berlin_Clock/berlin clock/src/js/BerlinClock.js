67efd90db3ea20c561ea26751c0ac6dcb657329f
Maxime Mercy <mxmercy@gmail.com>
2016-12-02 13:02:17 +0100

    berin clock challenge 1h

---
+++ Berlin_Clock/berlin clock/src/js/BerlinClock.js
@@ -0,0 +1,70 @@
+var BerlinClock = {
+    tick: function(){
+        var time = new Date(),
+            second = time.getSeconds();
+            minute = time.getMinutes();
+            hour = time.getHours();
+
+    this.updateHours()
+    this.updateMinutes()
+
+    },
+    updateSeconds: function(second){
+         $('.secondsCircle').toggleClass('yellow');
+    },
+
+    updateHours: function(){
+      nbr = Math.floor(hour/5)
+      nbrYellowFive = $('#hoursOfFive .yellow').size;
+      if (nbr != nbrYellowFive) {
+        if (nbr < nbrYellowFive ) {
+          $("#hoursOfFive .ticker").removeClass('yellow');
+          }
+          $("#hoursOfFive .ticker:lt("+nbr+")").addClass('yellow');
+      }
+
+      nbrRest = hour - nbr*5
+      nbrYellowFiveOne = $('#hoursOfOne .yellow').size();
+      if (nbrRest != nbrYellowFiveOne) {
+        if (nbrRest < nbrYellowFiveOne ) {
+          $("#hoursOfOne .ticker").removeClass('yellow');
+          }
+          $("#hoursOfOne .ticker:lt("+nbrRest+")").addClass('yellow');
+      }
+
+    },
+
+    updateMinutes: function(){
+        nbr = Math.floor(minute/5)
+        nbrYellowFive = $('#minutesOfFive .yellow').size;
+        if (nbr != nbrYellowFive) {
+          if (nbr < nbrYellowFive ) {
+            $("#minutesOfFive .ticker").removeClass('yellow');
+            }
+            $("#minutesOfFive .ticker:lt("+nbr+")").addClass('yellow');
+        }
+
+        nbrRest = minute - nbr*5
+        nbrYellowFiveOne = $('#minutesOfOne .yellow').size();
+        if (nbrRest != nbrYellowFiveOne) {
+          if (nbrRest < nbrYellowFiveOne ) {
+            $("#minutesOfOne .ticker").removeClass('yellow');
+            }
+            $("#minutesOfOne .ticker:lt("+nbrRest+")").addClass('yellow');
+        }
+
+    },
+
+    updateClock: function(hour, minute, second){
+
+    },
+
+    reset: function(){
+
+    }
+}
+
+setInterval(function(){
+      BerlinClock.tick()
+      BerlinClock.updateSeconds()
+ },1000);
